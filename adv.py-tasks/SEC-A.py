4207c537dae80a6ee7a9882a1e53f7ac52a1e5a1
Rajdeep <rajdeepmajhi077@gmail.com>
2026-03-24 17:24:08 +0530

    Add files via upload

---
+++ adv.py-tasks/SEC-A.py
@@ -0,0 +1,28 @@
+'''1. What will be the output?
+ x = [1,2,3]
+ print(x * 2)
+ a) [1,2,3,1,2,3]
+ b) Error
+ c) [2,4,6]
+ d) None'''
+# ans:-[1,2,3,1,2,3]
+'''2. What is the output?
+ print(bool(""))
+ a) True
+ b) False
+ c) Error
+ d) None'''
+#ans:-False
+
+'''3. Which is mutable?
+ a) tuple
+ b) string
+ c) list
+ d) int'''
+#ans:-list
+'''4. What will be output?
+ print(10 == 10.0)
+ a) True
+ b) False'''
+#ans:-false
+
---
+++ adv.py-tasks/SEC-B.py
@@ -0,0 +1,27 @@
+'''a = [1,2,3]
+ b = a
+ b.append(4)
+ print(a)'''
+#ans:-[1, 2, 3, 4]
+
+
+'''def func(x=[]):
+ x.append(1)
+ return x
+ print(func())
+ print(fun) '''
+#ans:-1
+
+'''for i in range(5):
+     if i == 3:
+ break
+ print(i)'''
+#ans:-0 1 2
+
+'''try:
+ print(10/0)
+ except:
+ print("Error")
+ finally:
+ print("Done")'''
+#ans:-Error Done
